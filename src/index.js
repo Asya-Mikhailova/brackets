877e8ff5a02066859cb0b212d63aa19fe7d0f5fb
Asya-Mikhailova <54442754+Asya-Mikhailova@users.noreply.github.com>
2020-03-17 14:49:01 +0100

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = Array.from(str);
+
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++ ){
+      if(array[i] == bracketsConfig[j][0] && array[i+1] == bracketsConfig[j][1]){
+        array.splice(i ,2);
+        i = -1;
+      }
+    }
+    if(array.length == 0){
+      return true;
+    }
+  
+  }
+  
+    return false;
+
 }
